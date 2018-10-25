7e4410637a026d2528565546f45434ad7eaade1b
ksures56@ford.com <ksures56@ford.com>
2018-10-25 10:19:44 -0400

    multiple Jars

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ServiceCredentials.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ServiceCredentials.java
@@ -2,11 +2,13 @@ package org.superbiz.moviefun;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class ServiceCredentials {
 
